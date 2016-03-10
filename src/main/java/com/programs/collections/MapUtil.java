2849d1f9db469df14b4076f295583ce75c354f7f
lakshmana4a8 <lakshmana4a8@gmail.com>
2016-03-10 19:07:21 +0530

    sort map

---
+++ src/main/java/com/programs/collections/MapUtil.java
@@ -0,0 +1,50 @@
+package com.programs.collections;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class MapUtil{
+	public static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+		
+	    List<Map.Entry<K, V>> list = new LinkedList<Map.Entry<K, V>>(map.entrySet());
+	    
+	    Collections.sort( list, new Comparator<Map.Entry<K, V>>() {
+	        public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
+	            return (o1.getValue()).compareTo(o2.getValue());
+	        }
+	    });
+	
+	    Map<K, V> result = new LinkedHashMap<K, V>();
+	    
+	    for (Map.Entry<K, V> entry : list) {
+	        result.put(entry.getKey(), entry.getValue());
+	    }
+	    
+	    return result;
+	}
+
+	public static <K, V extends Comparable<? super V>> Map<K, V> sortByKey(Map<K, V> map) {
+
+	    List<Map.Entry<K, V>> list = new LinkedList<Map.Entry<K, V>>(map.entrySet());
+	    System.out.println(": "+list);
+	    Collections.sort( list, new Comparator<Map.Entry<K, V>>() {
+	        public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
+	        	String a = (String) o1.getKey();
+	        	String b = (String) o2.getKey();
+	            return a.compareTo(b);
+	        }
+	    });
+	
+	    Map<K, V> result = new LinkedHashMap<K, V>();
+	    
+	    for (Map.Entry<K, V> entry : list) {
+	        result.put(entry.getKey(), entry.getValue());
+	    }
+	    
+	    return result;
+	}
+}
--- src/main/java/com/programs/collections/TestHashMap.java
+++ src/main/java/com/programs/collections/TestHashMap.java
@@ -7,21 +7,19 @@ import java.util.Map.Entry;
 
 public class TestHashMap {
 	public static void main(String[] args) {
-		Map<String, String> map = new HashMap<String, String>();
-		map.put("1", "11");
-		
-		String aa = map.put("2", "22");
-		System.out.println(aa);
-		String aa1 = map.put("2", "22");
-		System.out.println(aa1);
-		
-		map.put("3", "33");
-		map.put("3", "44");
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		map.put("1", 11);
+		map.put("2", 55);
+		map.put("3", 33);
+		map.put("4", 1);
+		map.put("5", 1);
 		System.out.println(map);
+		System.out.println(MapUtil.sortByValue(map));
+		System.out.println(MapUtil.sortByKey(map));
 		
-		Iterator<Entry<String, String>> iterator = map.entrySet().iterator();
+		Iterator<Entry<String, Integer>> iterator = map.entrySet().iterator();
 		while (iterator.hasNext()) {
-			Map.Entry<String, String> entry = (Map.Entry<String, String>) iterator.next();
+			Map.Entry<String, Integer> entry = (Map.Entry<String, Integer>) iterator.next();
 			System.out.println(entry.getKey()+" : "+entry.getValue());
 		}
 	}
